bc4fa944c83e96495cefece3259fee8f7f768f81
projeta618 <projeta618@gmail.com>
2024-02-18 00:00:00 -0300

    feat: Add Category and CategoryTranslation models with admin functionality
    Why: This commit adds the Category and CategoryTranslation models with their respective fields and relationships. It also includes the necessary admin functionality for managing these models.
    
    Changes:
    - Added Category model with fields 'activated', 'created_on', 'updated_on', 'icon', 'name', and 'description'
    - Added CategoryTranslation model with fields 'activated', 'created_on', 'updated_on', 'icon', 'language', 'name', 'description

--- admin.py
+++ admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from category import models
 
-# Register your models here.
+
+class CategoryTranslationInline(admin.StackedInline):
+    """
+        @description: JobCategoryInline
+    """
+
+    model = models.CategoryTranslation
+    extra = 0
+
+    fields = [
+        'language',
+        'name',
+        'description',
+    ]
+    formfield_overrides = {
+
+    }
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'icon')
+    inlines = [CategoryTranslationInline]
--- models.py
+++ models.py
@@ -1,3 +1,83 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from kernel.models.base_metadata_model import BaseMetadataModel
+from kernel.models.serialize import serializer__serialize__, serializer__init__
+from django.forms.models import model_to_dict
+
+class CategoryTranslation(BaseMetadataModel):
+    icon = models.ImageField(
+        upload_to='category/icons/',
+        blank=True, 
+        null=True
+    )
+
+    language = models.CharField(
+        'language',
+        max_length=255,
+        default='fr',
+        choices=(
+            ('fr', 'fr'),
+        ),
+    )
+
+    name = models.CharField(
+        max_length=255, 
+    )
+
+    description = models.TextField(blank=True, null=True)
+
+    translateObject = models.ForeignKey(
+        'Category',
+        on_delete=models.CASCADE,
+        # related_name='category_translates',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self) -> str:
+        return self.language
+    
+    def serialize(self, request):
+        """
+            @description: 
+        """
+        serialize = model_to_dict(self)
+        return serialize
+
+
+class Category(BaseMetadataModel):
+    translation_model = CategoryTranslation
+
+    @serializer__init__
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    icon = models.ImageField(
+        upload_to='category/icons/',
+        blank=True, 
+        null=True
+    )
+
+    name = models.CharField(
+        max_length=255, 
+        unique=True,
+    )
+
+    description = models.TextField(
+        blank=True, 
+        null=True
+    )
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+    @serializer__serialize__
+    def serialize(self, request, **kwargs):
+        """
+            @description: 
+        """
+        serialize = model_to_dict(self)
+        return serialize
